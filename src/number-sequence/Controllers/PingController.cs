d48a215940a7e5f03d2eecd4e54ecc4cf7e30d57
Tom Postler <tom@postler.me>
2020-10-10 23:12:31 -0500

    ping

---
+++ src/number-sequence/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace number_sequence.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public sealed class PingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return this.Ok();
+        }
+    }
+}
