3825076e7a1224cc1cad6340b0747695c71cbec6
Tom Postler <tom@postler.me>
2020-10-11 19:24:01 -0500

    enable app insights

--- src/number-sequence/Startup.cs
+++ src/number-sequence/Startup.cs
@@ -19,6 +19,7 @@ namespace number_sequence
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
