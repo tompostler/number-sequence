c43f0d2afb75dc50c678b9b8cb3e71f50ad7ad38
Tom Postler <tom@postler.me>
2020-10-12 17:02:57 -0500

    meek throttle progress

---
+++ src/number-sequence/Filters/Throttle.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace number_sequence.Filters
+{
+    public sealed class Throttle : ActionFilterAttribute
+    {
+        public Throttle()
+        {
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+        }
+    }
+}
