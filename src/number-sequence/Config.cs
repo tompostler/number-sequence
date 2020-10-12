dc54246c5b5a3ccee8e88764411f6d236718a522
Tom Postler <tom@postler.me>
2020-10-11 19:44:43 -0500

    connect to cosmos

---
+++ src/number-sequence/Config.cs
@@ -0,0 +1,11 @@
+﻿namespace number_sequence
+{
+    public static class Config
+    {
+        public sealed class CosmosDB
+        {
+            public string Endpoint { get; set; }
+            public string Key { get; set; }
+        }
+    }
+}
