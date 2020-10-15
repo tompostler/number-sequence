1b0bb273f985bd1862de250b58113e029c33cc07
Tom Postler <tom@postler.me>
2020-10-14 23:13:41 -0500

    some more

---
+++ src/Models/AccountTier.cs
@@ -0,0 +1,28 @@
+﻿namespace TcpWtf.NumberSequence.Models
+{
+    /// <summary>
+    /// The tier for an account. This plays into the limit for numbers, requests, and other resources.
+    /// </summary>
+    public enum AccountTier
+    {
+        /// <summary>
+        /// Small tier should be fine for most usage scenarios.
+        /// </summary>
+        Small,
+
+        /// <summary>
+        /// Medium tier provides more burstability on requests with some additional resources.
+        /// </summary>
+        Medium,
+
+        /// <summary>
+        /// Large tier allows a sizeable number of resources.
+        /// </summary>
+        Large,
+
+        /// <summary>
+        /// ALL THE RESOURCES POSSIBLE!
+        /// </summary>
+        Infinite
+    }
+}
