ccb0a2086ef2c9bf954684ed9d74b35d678d41c1
Tom Postler <tom@postler.me>
2022-10-17 09:40:21 -0500

    ProgressLogger updates

--- src/number-sequence/Utilities/ProgressLogger.cs
+++ src/number-sequence/Utilities/ProgressLogger.cs
@@ -8,8 +8,12 @@ namespace number_sequence.Utilities
     {
         private readonly long expectedLength;
         private readonly ILogger logger;
+
         private readonly object reportingLock = new();
         private readonly Queue<(DateTimeOffset, long)> byteProgress = new(3);
+        private readonly DateTimeOffset started = DateTimeOffset.UtcNow;
+
+        private DateTimeOffset? lastReported = default;
 
         public ProgressLogger(long expectedLength, ILogger logger)
         {
@@ -17,26 +21,21 @@ namespace number_sequence.Utilities
             this.logger = logger;
         }
 
-        private readonly DateTimeOffset started = DateTimeOffset.UtcNow;
-        private DateTimeOffset? lastReported = default;
-
+        /// <summary>
+        /// Log the current progress. Will only emit values once every 3 seconds regardless of the number of calls.
+        /// </summary>
         public void Report(long value)
         {
-            lock (this.reportingLock)
-            {
-                const int intervalSecondsToReport = 2;
-                const int lookbackSliceCount = 5;
-                DateTimeOffset now = DateTimeOffset.UtcNow;
+            const int intervalSecondsToReport = 3;
+            const int lookbackSliceCount = 5;
+            DateTimeOffset now = DateTimeOffset.UtcNow;
 
-                // If we appear to be at the end, also add some summary info
-                if (value >= this.expectedLength)
-                {
-                    TimeSpan duration = now - this.started;
-                    decimal totalSeconds = (decimal)duration.TotalSeconds;
-                    this.logger.LogInformation($"Progress: {BytesToFriendlyString(value),9}/{BytesToFriendlyString(this.expectedLength)} ({1.0 * value / this.expectedLength:p}). Averaged {BytesToFriendlyBitString(value / totalSeconds)}ps over {duration:mm\\:ss}.");
-                }
-                // Else if we've just started or once every couple of seconds.
-                else if (!this.lastReported.HasValue || (now - this.lastReported.Value).TotalSeconds > intervalSecondsToReport)
+            // If we've just started or once every couple of seconds, then continue with reporting
+            if (!this.lastReported.HasValue || (now - this.lastReported.Value).TotalSeconds > intervalSecondsToReport)
+            {
+                // Locks during the calculation/generation of the actual report in order to ensure only one thread is reporting on the object at a time.
+                // If multiple thread reports came in here, they'll just log a couple times which is fine. So we're not re-checking the condition to log.
+                lock (this.reportingLock)
                 {
                     if (this.byteProgress.Count >= lookbackSliceCount)
                     {
@@ -55,44 +54,52 @@ namespace number_sequence.Utilities
             }
         }
 
+        public void ReportComplete()
+        {
+            // Report 100% based on expected length
+            TimeSpan duration = DateTimeOffset.UtcNow - this.started;
+            decimal totalSeconds = (decimal)duration.TotalSeconds;
+            this.logger.LogInformation($"Progress: {BytesToFriendlyString(this.expectedLength),9}/{BytesToFriendlyString(this.expectedLength)} ({1.0:p}). Averaged {BytesToFriendlyBitString(this.expectedLength / totalSeconds)}ps over {duration:mm\\:ss}.");
+        }
+
         private static string BytesToFriendlyString(decimal bytes)
         {
-            if (bytes < 1_000_000)
+            if (bytes < 1_048_576)
             {
-                return $"{bytes / 1_000:0.00}KiB";
+                return $"{bytes / 1_024:0.00}KiB";
             }
-            else if (bytes >= 1_000_000 && bytes < 1_000_000_000)
+            else if (bytes >= 1_048_576 && bytes < 1_073_741_824)
             {
-                return $"{bytes / 1_000_000:0.00}MiB";
+                return $"{bytes / 1_048_576:0.00}MiB";
             }
-            else if (bytes >= 1_000_000_000 && bytes < 1_000_000_000_000)
+            else if (bytes >= 1_073_741_824 && bytes < 1_099_511_627_776)
             {
-                return $"{bytes / 1_000_000_000:0.00}GiB";
+                return $"{bytes / 1_073_741_824:0.00}GiB";
             }
             else
             {
-                return $"{bytes / 1_000_000_000_000:0.00}TiB";
+                return $"{bytes / 1_099_511_627_776:0.00}TiB";
             }
         }
 
         private static string BytesToFriendlyBitString(decimal bytes)
         {
             decimal bits = bytes * 8;
-            if (bits < 1_048_576)
+            if (bits < 1_000_000)
             {
-                return $"{bits / 1_024:0.00}Kb";
+                return $"{bits / 1_000:0.00}Kb";
             }
-            else if (bits >= 1_048_576 && bits < 1_073_741_824)
+            else if (bits >= 1_000_000 && bits < 1_000_000_000)
             {
-                return $"{bits / 1_048_576:0.00}Mb";
+                return $"{bits / 1_000_000:0.00}Mb";
             }
-            else if (bits >= 1_073_741_824 && bits < 1_099_511_627_776)
+            else if (bits >= 1_000_000_000 && bits < 1_000_000_000_000)
             {
-                return $"{bits / 1_073_741_824:0.00}Gb";
+                return $"{bits / 1_000_000_000:0.00}Gb";
             }
             else
             {
-                return $"{bits / 1_099_511_627_776:0.00}Tb";
+                return $"{bits / 1_000_000_000_000:0.00}Tb";
             }
         }
     }
