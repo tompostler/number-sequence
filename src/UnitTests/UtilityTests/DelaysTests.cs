70d76c7d88175d761eacdff54959c7caeaf48452
Tom Postler <tom@postler.me>
2022-10-17 08:40:37 -0500

    add delays with unit tests

---
+++ src/UnitTests/UtilityTests/DelaysTests.cs
@@ -0,0 +1,70 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using number_sequence.Utilities;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace number_sequence.UnitTests.UtilityTests
+{
+    [TestClass]
+    public sealed class DelaysTests
+    {
+        private Delays.DelayImpelmentation delay;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            this.delay = new Delays.DelayImpelmentation();
+        }
+
+        [TestMethod]
+        public async Task DelayWithNoInterruptShouldDelay()
+        {
+            // Arrange
+            var delay = TimeSpan.FromSeconds(1);
+            var sw = Stopwatch.StartNew();
+
+            // Act
+            await this.delay.Delay(delay, CancellationToken.None);
+
+            // Assert
+            _ = sw.Elapsed.Should().BeCloseTo(delay, TimeSpan.FromSeconds(0.1));
+        }
+
+        [TestMethod]
+        public async Task DelayWithInterruptShouldDelayUntilInterrupt()
+        {
+            // Arrange
+            var delay = TimeSpan.FromSeconds(1);
+            var sw = Stopwatch.StartNew();
+
+            // Act
+            Task delayTask = this.delay.Delay(delay, CancellationToken.None);
+            await Task.Delay((int)delay.TotalMilliseconds / 2);
+            this.delay.CancelDelay();
+            await delayTask;
+
+            // Assert
+            _ = sw.Elapsed.Should().BeCloseTo(delay / 2, TimeSpan.FromSeconds(0.1));
+            _ = delayTask.IsCompletedSuccessfully.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public async Task DelayWithCancellationShouldThrow()
+        {
+            // Arrange
+            var delay = TimeSpan.FromSeconds(1);
+            var sw = Stopwatch.StartNew();
+            var cts = new CancellationTokenSource(delay / 2);
+
+            // Act
+            Func<Task> act = () => this.delay.Delay(delay, cts.Token);
+
+            // Assert
+            _ = await act.Should().ThrowAsync<OperationCanceledException>();
+            _ = sw.Elapsed.Should().BeCloseTo(delay / 2, TimeSpan.FromSeconds(0.1));
+        }
+    }
+}
---
+++ src/number-sequence/Utilities/Delays.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using System.Threading;
+using System;
+
+namespace number_sequence.Utilities
+{
+    public sealed class Delays
+    {
+        public sealed class DelayImpelmentation
+        {
+            private TaskCompletionSource earlyTaskCompletionSource;
+
+            /// <summary>
+            /// Gracefully cancel the delay.
+            /// </summary>
+            public void CancelDelay() => this.earlyTaskCompletionSource?.TrySetResult();
+
+            /// <summary>
+            /// Intended to only be called by one location at a time and not in parallel.
+            /// </summary>
+            public async Task Delay(TimeSpan delay, CancellationToken cancellationToken)
+            {
+                this.earlyTaskCompletionSource = new(TaskCreationOptions.RunContinuationsAsynchronously);
+
+                var cts = new CancellationTokenSource();
+                var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(cts.Token, cancellationToken);
+
+                var delayTask = Task.Delay(delay, linkedCts.Token);
+
+                // Whichever one returned, cancel the other
+                Task returnedTask = await Task.WhenAny(delayTask, this.earlyTaskCompletionSource.Task);
+                if (returnedTask == delayTask)
+                {
+                    _ = this.earlyTaskCompletionSource.TrySetCanceled(CancellationToken.None);
+                    this.earlyTaskCompletionSource = null;
+                    await delayTask;
+                }
+                else
+                {
+                    cts.Cancel();
+                }
+            }
+        }
+    }
+}
