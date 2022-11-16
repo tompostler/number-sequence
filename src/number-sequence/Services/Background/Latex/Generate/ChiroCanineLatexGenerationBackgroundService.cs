c89d24eb12bda42d60a84f43314c27c9d6ec0206
Tom Postler <tom@postler.me>
2022-11-15 21:49:35 -0600

    log info before exception

--- src/number-sequence/Services/Background/Latex/Generate/ChiroCanineLatexGenerationBackgroundService.cs
+++ src/number-sequence/Services/Background/Latex/Generate/ChiroCanineLatexGenerationBackgroundService.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using number_sequence.DataAccess;
+using number_sequence.Extensions;
 using number_sequence.Models;
 using number_sequence.Utilities;
 using System;
@@ -126,18 +127,28 @@ namespace number_sequence.Services.Background.Latex.Generate
 
             string customAppend(string existing, string prefix, int index)
             {
-                if (index > row.Length)
+                try
                 {
-                    return string.Empty;
+
+                    if (index > row.Length)
+                    {
+                        return string.Empty;
+                    }
+                    else if (!string.IsNullOrWhiteSpace(row[index]))
+                    {
+                        return (string.IsNullOrWhiteSpace(existing) ? string.Empty : ", ")
+                            + $"{prefix} {row[index]}".Trim().EscapeForLatex();
+                    }
+                    else
+                    {
+                        return string.Empty;
+
+                    }
                 }
-                else if (!string.IsNullOrWhiteSpace(row[index]))
+                catch
                 {
-                    return (string.IsNullOrWhiteSpace(existing) ? string.Empty : ", ")
-                        + $"{prefix} {row[index]}".Trim().EscapeForLatex();
-                }
-                else
-                {
-                    return string.Empty;
+                    this.logger.LogInformation(new { existing, prefix, index }.ToJsonString());
+                    throw;
                 }
             }
 
