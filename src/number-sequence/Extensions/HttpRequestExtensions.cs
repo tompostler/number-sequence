bf25d87a15569a7457478990a868b2a115545464
Tom Postler <tom@postler.me>
2023-03-07 20:56:47 -0600

    missed a file

---
+++ src/number-sequence/Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace number_sequence.Extensions
+{
+    public static class HttpRequestExtensions
+    {
+        public static IPAddress GetIPAddress(this HttpRequest @this)
+        {
+            if (@this.Headers.ContainsKey("X-Forwarded-For") && IPAddress.TryParse(@this.Headers["X-Forwarded-For"], out IPAddress address))
+            {
+                return address;
+            }
+            return default;
+        }
+    }
+}
