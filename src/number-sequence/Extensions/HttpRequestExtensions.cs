2d5e2da66fdcdd9cb955c9e9b420b657a761cfa7
Tom Postler <tom@postler.me>
2026-01-31 21:01:15 -0600

    put behind cloudflare

--- src/number-sequence/Extensions/HttpRequestExtensions.cs
+++ src/number-sequence/Extensions/HttpRequestExtensions.cs
@@ -1,13 +1,25 @@
-﻿namespace number_sequence.Extensions
+﻿using Microsoft.Extensions.Primitives;
+
+namespace number_sequence.Extensions
 {
     public static class HttpRequestExtensions
     {
         public static string GetClientIPAddress(this HttpRequest @this)
         {
-            const string headerName = "X-Client-IP";
-            return @this.Headers.ContainsKey(headerName)
-                ? (string)@this.Headers[headerName]
-                : @this.HttpContext.Connection.RemoteIpAddress?.ToString();
+            // Cloudflare, https://developers.cloudflare.com/fundamentals/reference/http-headers/#cf-connecting-ip
+            if (@this.Headers.TryGetValue("CF-Connecting-IP", out StringValues cloudflareIp))
+            {
+                return cloudflareIp;
+            }
+            // Azure App Service
+            else if (@this.Headers.TryGetValue("X-Client-IP", out StringValues appServiceIp))
+            {
+                return appServiceIp;
+            }
+            else
+            {
+                return @this.HttpContext.Connection.RemoteIpAddress?.ToString();
+            }
         }
     }
 }
